48eaa44300fbdf9d43403469dde099467354c7a0
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-11 11:37:55 -0400

    Allow editing of existing books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,23 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully updated"
+    else
+      redirect_to book_path(@book), alert: "Error: #{@book.errors.full_messages.join(", ")}"
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "meetings#index"
 
-  resources :books, only: [ :index ]
+  resources :books, only: [ :index, :edit, :update ]
 end
