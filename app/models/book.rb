d0ff0e8dbd31b139ba1103f17306432e7f6b5885
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-11 11:00:18 -0400

    Force StoryGraph URLs

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, format: { with: /\Ahttps:\/\/app\.thestorygraph\.com\/books\//, message: "must link to StoryGraph" }
 end
