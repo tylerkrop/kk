7d5280c14e39c268c91094af4f265287a344283d
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-11 15:12:56 -0400

    Gate forms behind auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Redirects the user to the previous page after sign-in
+  # https://github.com/heartcombo/devise/wiki/How-To:-%5BRedirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update%5D
+  after_action :store_action
+  def store_action
+    return unless request.get?
+    if request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr? # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
+  end
 end
