f949c199e1daed1e0a46060d4b2981cb2eb0f03f
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-11 20:16:10 -0400

    Create way to delete votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,4 +4,13 @@ class VotesController < ApplicationController
   def index
     @votes = Vote.where(user: current_user).order(:position)
   end
+
+  def destroy
+    @vote = current_user.votes.find(params[:id])
+    if @vote.destroy
+      redirect_to votes_path, notice: "Vote was successfully deleted"
+    else
+      redirect_to votes_path, alert: "Error: #{@vote.errors.full_messages.join(", ")}"
+    end
+  end
 end
