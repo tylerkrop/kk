9e6b6c9cd9eca290fdbd1a574c98655d25f22cd3
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-15 17:15:36 -0400

    Fix Brakeman regex issue

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
-  validates :url, presence: true, format: { with: /\Ahttps:\/\/app\.thestorygraph\.com\/books\//, message: "must link to StoryGraph" }
+  validates :url, presence: true, format: { with: /\Ahttps:\/\/app\.thestorygraph\.com\/books\/.*\z/, message: "must link to StoryGraph" }
 end
