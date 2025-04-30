b50bed51de8f2cdfde4337493dd699a335e98ddb
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-04-29 21:24:41 -0400

    Codify the algorithm for printing the current book points

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,19 @@ class Vote < ApplicationRecord
   validates :user_id, presence: true
   validates :book_id, presence: true
   acts_as_list top_of_list: 0, scope: :user
+
+  # Borda Count (Bottom-Tie) Ranking Algorithm
+  def current_points
+    titles = Vote.all.map do |vote|
+      vote.book.title
+    end.uniq
+    k = titles.size
+    points = Hash.new(0)
+    Vote.all.each do |vote|
+      points[vote.book.title] += k - vote.position
+    end
+    points.sort_by { |k, v|-v }.each do |k, v|
+      puts "#{k}: #{v}"
+    end
+  end
 end
