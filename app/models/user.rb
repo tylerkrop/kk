7846ba1737b13ee87ff462f08f0e10bfa2eb3547
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-05-09 10:59:15 -0400

    Remove comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable,
          :omniauthable, omniauth_providers: %i[discord]
   has_many :votes, dependent: :destroy
