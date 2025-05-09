9cf6e5f90ac1d105d03d8e2cf861293a2014d1ce
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-05-09 10:22:22 -0400

    Unit tests for the meetings model

--- test/models/meeting_test.rb
+++ test/models/meeting_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class MeetingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid meeting" do
+    meeting = Meeting.new(
+      book: books(:one),
+      meeting_time: Time.now + 1.day,
+      reading_expectations: "Read chapters 1-3"
+    )
+    assert meeting.valid?
+  end
+
+  test "invalid without book" do
+    meeting = Meeting.new(
+      meeting_time: Time.now + 1.day,
+      reading_expectations: "Read chapters 1-3"
+    )
+    assert_not meeting.valid?
+    assert_includes meeting.errors[:book], "must exist"
+  end
+
+  test "invalid without meeting_time" do
+    meeting = Meeting.new(
+      book: books(:one),
+      reading_expectations: "Read chapters 1-3"
+    )
+    assert_not meeting.valid?
+    assert_includes meeting.errors[:meeting_time], "can't be blank"
+  end
+
+  test "invalid without reading_expectations" do
+    meeting = Meeting.new(
+      book: books(:one),
+      meeting_time: Time.now + 1.day
+    )
+    assert_not meeting.valid?
+    assert_includes meeting.errors[:reading_expectations], "can't be blank"
+  end
 end
