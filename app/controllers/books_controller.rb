7c9456a2ffda7d84149cd28ba39eb67dcf6cc3bf
Tyler Kropiewnicki <tylerkrop@gmail.com>
2025-05-27 20:26:13 -0400

    Order books by created date

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @books = Book.with_visible.order(:title).page params[:page]
+    @books = Book.with_visible.order(created_at: :desc).page params[:page]
     @current_user_votes_book_ids = current_user.votes.pluck(:book_id) if user_signed_in?
   end
 
